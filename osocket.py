348173fbe5727da34a067b388d5fd76999c99c96
omiddavoodi <ariestinak@gmail.com>
2015-09-21 15:00:06 +0430

    Create osocket.py

---
+++ osocket.py
@@ -0,0 +1,78 @@
+import socket
+
+class OSocketException(Exception):
+    pass
+
+class OSocketSendMustBeBytes(Exception):
+    pass
+
+class OSocket:
+    def __init__(self, timeout=60):
+        self.socket = socket.socket()
+        self.socket.settimeout(timeout)
+        self.buffer = b''
+        
+    def connect(self, host, port):
+        self.socket.connect((host, port))
+
+    def settimeout(self):
+        self.socket.settimeout()
+
+    def readmessage(self):
+        ret = self.buffer
+        lengthFound = False
+        length = 0
+        lastLengthByte = 0
+        lastMask = 1
+        
+        while (True):
+            rawbytes = self.socket.recv(512)
+            if (rawbytes == b''):
+                raise (OSocketException)
+            
+            ret += rawbytes
+            
+            while (not lengthFound and len(ret) > lastLengthByte):
+                curlbyte = ret[lastLengthByte]
+                if (curlbyte >= 128):
+                    length += lastMask * (curlbyte % 128)
+                    lastMask *= 128
+                    lastLengthByte += 1
+                else:
+                    length += lastMask * (curlbyte % 128)
+                    lastLengthByte += 1
+                    lengthFound = True
+            if (lengthFound):
+                if (len(ret) - lastLengthByte == length):
+                    self.buffer = ret[length:]
+                    return ret[lastLengthByte:length+1]
+
+    def sendmessage(self, message):
+        if (type(message) != bytes):
+            raise (OSocketSendMustBeBytes)
+
+        k = len(message)
+
+        l = []
+
+        while (k > 0):
+            t = k % 128
+            k = k // 128
+            if (k > 0):
+                t += 128
+
+            l.append(t)
+
+        self.socket.send(bytes(l) + message)
+
+    def bind(self, a):
+        return self.socket.bind(a)
+    
+    def listen(self, num):
+        return self.socket.listen(num)
+
+    def accept(self):
+        return self.socket.accept()
+
+    def close(self):
+        return self.socket.close()
